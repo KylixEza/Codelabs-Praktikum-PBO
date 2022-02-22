9cb356d4816260d418108fc141fd08e28a406bb7
ACER <loilyxtra@gmail.com>
2022-02-22 10:40:45 +0700

    Praktikum 1

---
+++ praktikum1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Animal singa = new Animal();
+        Animal kucing = new Animal();
+
+        Animal lion = new Animal();
+        Animal cat = new Animal();
+        Animal ant = new Animal();
+        Animal bird = new Animal();
+
+        Animal black_panther = new Animal(); //1 x
+        Animal BlackPanther = new Animal();  //2 x
+        Animal blackPanther = new Animal();  //3 (camelCase) v
+        Animal Blackpanther = new Animal();  //4 x
+    }
+}
