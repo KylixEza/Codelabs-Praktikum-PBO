9c91e001a6d43e8a0921a0d2b76bc10eeb8d634a
ACER <loilyxtra@gmail.com>
2022-04-07 14:24:41 +0700

    Praktikum 6.1 (before UTP)

---
+++ praktikum6.1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Math math = new Math();
+        Math.number = 1;
+        System.out.println(Math.add(2, 3));
+        //math.index = 1;
+    }
+}
---
+++ praktikum6.1/src/Math.java
@@ -0,0 +1,8 @@
+public class Math {
+    static int number = 0;
+    final int index = 0;
+
+    static int add(int firstNumber, int secondNUmber) {
+        return firstNumber + secondNUmber;
+    }
+}
